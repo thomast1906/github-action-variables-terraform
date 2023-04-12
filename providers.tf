3057401ef58753e566fd1cae38d4a7a9c6daf355
Thomas Thornton <>
2023-04-12 15:17:08 +0100

    hmm

--- providers.tf
+++ providers.tf
@@ -5,16 +5,9 @@ terraform {
     container_name       = "terraformgithubexample2"
     key                  = "terraform.tfstate"
   }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
 }
 
 provider "azurerm" {
   features {}
 }
 
-data "azurerm_client_config" "current" {}
